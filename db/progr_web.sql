f26c4325a47ecfc9e7cc195e4e570e7d6f17ed9f
Daniele2712 <daniele27_12@hotmail.it>
2018-05-21 19:22:47 +0200

    caricato database

---
+++ db/progr_web.sql
@@ -0,0 +1,550 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mag 21, 2018 alle 19:21
+-- Versione del server: 10.1.30-MariaDB
+-- Versione PHP: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `progr_web`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `attributi`
+--
+
+CREATE TABLE `attributi` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `filtrabile` tinyint(1) NOT NULL,
+  `nome_categoria` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `carrelli`
+--
+
+CREATE TABLE `carrelli` (
+  `id` int(11) NOT NULL,
+  `totale` float NOT NULL,
+  `valuta` enum('EUR','USD','GBP','BTC','JPY') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `carte`
+--
+
+CREATE TABLE `carte` (
+  `id` int(11) NOT NULL,
+  `numero` int(20) NOT NULL,
+  `cvv` int(5) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `cognome` varchar(100) NOT NULL,
+  `data_scadenza` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `nome` varchar(50) NOT NULL,
+  `padre` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `comuni`
+--
+
+CREATE TABLE `comuni` (
+  `id` int(11) NOT NULL,
+  `CAP` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `provincia` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `dati_anagrafici`
+--
+
+CREATE TABLE `dati_anagrafici` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `cognome` varchar(100) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `datanascita` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `gestori`
+--
+
+CREATE TABLE `gestori` (
+  `id` int(11) NOT NULL,
+  `id_datianagrafici` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `indirizzi`
+--
+
+CREATE TABLE `indirizzi` (
+  `id` int(11) NOT NULL,
+  `id_comune` int(11) NOT NULL,
+  `via` varchar(200) NOT NULL,
+  `civico` int(11) NOT NULL,
+  `note` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `indirizzi_preferiti`
+--
+
+CREATE TABLE `indirizzi_preferiti` (
+  `id` int(11) NOT NULL,
+  `id_utente_r` int(11) NOT NULL,
+  `id_indirizzo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `locazione` enum('M','O','C','') NOT NULL,
+  `id_prodotto` int(11) NOT NULL,
+  `totale` float NOT NULL,
+  `valuta` enum('EUR','USD','GBP','BTC','JPY') NOT NULL,
+  `quantita` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `magazzini`
+--
+
+CREATE TABLE `magazzini` (
+  `id` int(11) NOT NULL,
+  `id_gestore` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `opzioni`
+--
+
+CREATE TABLE `opzioni` (
+  `id` int(11) NOT NULL,
+  `id_attributo` int(11) NOT NULL,
+  `valore` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `ordini`
+--
+
+CREATE TABLE `ordini` (
+  `id` int(11) NOT NULL,
+  `tipo_pagamento` enum('Carta','Paypal','Bitcoin','Contrassegno') NOT NULL,
+  `id_m_pagamento` int(11) DEFAULT NULL,
+  `id_indirizzo` int(11) NOT NULL,
+  `id_dati_anagrafici` int(11) NOT NULL,
+  `totale` float NOT NULL,
+  `valuta` enum('EUR','USD','GBP','BTC','JPY') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `pagamenti_preferiti`
+--
+
+CREATE TABLE `pagamenti_preferiti` (
+  `id` int(11) NOT NULL,
+  `id_utente_r` int(11) NOT NULL,
+  `tipo` enum('Carta','Paypal','Bitcoin','') NOT NULL,
+  `id_m_pagamento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `prodotti`
+--
+
+CREATE TABLE `prodotti` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `info` varchar(200) NOT NULL,
+  `descrizione` text NOT NULL,
+  `nome_categoria` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `utenti_registrati`
+--
+
+CREATE TABLE `utenti_registrati` (
+  `id` int(11) NOT NULL,
+  `id_datianagrafici` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `valori`
+--
+
+CREATE TABLE `valori` (
+  `id` int(11) NOT NULL,
+  `id_attributo` int(11) NOT NULL,
+  `id_prodotto` int(11) NOT NULL,
+  `id_opzione` int(11) DEFAULT NULL,
+  `valore` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `attributi`
+--
+ALTER TABLE `attributi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nome_categoria` (`nome_categoria`);
+
+--
+-- Indici per le tabelle `carrelli`
+--
+ALTER TABLE `carrelli`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `carte`
+--
+ALTER TABLE `carte`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`nome`),
+  ADD KEY `padre` (`padre`);
+
+--
+-- Indici per le tabelle `comuni`
+--
+ALTER TABLE `comuni`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `dati_anagrafici`
+--
+ALTER TABLE `dati_anagrafici`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `gestori`
+--
+ALTER TABLE `gestori`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_datianagrafici` (`id_datianagrafici`);
+
+--
+-- Indici per le tabelle `indirizzi`
+--
+ALTER TABLE `indirizzi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_comune` (`id_comune`);
+
+--
+-- Indici per le tabelle `indirizzi_preferiti`
+--
+ALTER TABLE `indirizzi_preferiti`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_utente_r` (`id_utente_r`),
+  ADD KEY `id_indirizzo` (`id_indirizzo`);
+
+--
+-- Indici per le tabelle `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`,`locazione`,`id_prodotto`),
+  ADD KEY `id_prodotto` (`id_prodotto`);
+
+--
+-- Indici per le tabelle `magazzini`
+--
+ALTER TABLE `magazzini`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_gestore` (`id_gestore`);
+
+--
+-- Indici per le tabelle `opzioni`
+--
+ALTER TABLE `opzioni`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_attributo` (`id_attributo`);
+
+--
+-- Indici per le tabelle `ordini`
+--
+ALTER TABLE `ordini`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_indirizzo` (`id_indirizzo`),
+  ADD KEY `id_dati_anagrafici` (`id_dati_anagrafici`);
+
+--
+-- Indici per le tabelle `pagamenti_preferiti`
+--
+ALTER TABLE `pagamenti_preferiti`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_utente_r` (`id_utente_r`);
+
+--
+-- Indici per le tabelle `prodotti`
+--
+ALTER TABLE `prodotti`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categoria` (`nome_categoria`);
+
+--
+-- Indici per le tabelle `utenti_registrati`
+--
+ALTER TABLE `utenti_registrati`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_datianagrafici` (`id_datianagrafici`);
+
+--
+-- Indici per le tabelle `valori`
+--
+ALTER TABLE `valori`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_prodotto` (`id_prodotto`),
+  ADD KEY `id_opzione` (`id_opzione`),
+  ADD KEY `id_attributo` (`id_attributo`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `attributi`
+--
+ALTER TABLE `attributi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `carrelli`
+--
+ALTER TABLE `carrelli`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `carte`
+--
+ALTER TABLE `carte`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `comuni`
+--
+ALTER TABLE `comuni`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `dati_anagrafici`
+--
+ALTER TABLE `dati_anagrafici`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `gestori`
+--
+ALTER TABLE `gestori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `indirizzi`
+--
+ALTER TABLE `indirizzi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `indirizzi_preferiti`
+--
+ALTER TABLE `indirizzi_preferiti`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `magazzini`
+--
+ALTER TABLE `magazzini`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `opzioni`
+--
+ALTER TABLE `opzioni`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `ordini`
+--
+ALTER TABLE `ordini`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `pagamenti_preferiti`
+--
+ALTER TABLE `pagamenti_preferiti`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `prodotti`
+--
+ALTER TABLE `prodotti`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `utenti_registrati`
+--
+ALTER TABLE `utenti_registrati`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `valori`
+--
+ALTER TABLE `valori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `attributi`
+--
+ALTER TABLE `attributi`
+  ADD CONSTRAINT `attributi_ibfk_1` FOREIGN KEY (`nome_categoria`) REFERENCES `categorie` (`nome`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `categorie`
+--
+ALTER TABLE `categorie`
+  ADD CONSTRAINT `categorie_ibfk_1` FOREIGN KEY (`padre`) REFERENCES `categorie` (`nome`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `indirizzi`
+--
+ALTER TABLE `indirizzi`
+  ADD CONSTRAINT `indirizzi_ibfk_1` FOREIGN KEY (`id_comune`) REFERENCES `comuni` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `indirizzi_preferiti`
+--
+ALTER TABLE `indirizzi_preferiti`
+  ADD CONSTRAINT `indirizzi_preferiti_ibfk_1` FOREIGN KEY (`id_indirizzo`) REFERENCES `indirizzi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `indirizzi_preferiti_ibfk_2` FOREIGN KEY (`id_utente_r`) REFERENCES `utenti_registrati` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`id_prodotto`) REFERENCES `prodotti` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `magazzini`
+--
+ALTER TABLE `magazzini`
+  ADD CONSTRAINT `magazzini_ibfk_1` FOREIGN KEY (`id_gestore`) REFERENCES `gestori` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `opzioni`
+--
+ALTER TABLE `opzioni`
+  ADD CONSTRAINT `opzioni_ibfk_1` FOREIGN KEY (`id_attributo`) REFERENCES `attributi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `ordini`
+--
+ALTER TABLE `ordini`
+  ADD CONSTRAINT `ordini_ibfk_1` FOREIGN KEY (`id_dati_anagrafici`) REFERENCES `dati_anagrafici` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ordini_ibfk_2` FOREIGN KEY (`id_indirizzo`) REFERENCES `indirizzi` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limiti per la tabella `pagamenti_preferiti`
+--
+ALTER TABLE `pagamenti_preferiti`
+  ADD CONSTRAINT `pagamenti_preferiti_ibfk_1` FOREIGN KEY (`id_utente_r`) REFERENCES `utenti_registrati` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `prodotti`
+--
+ALTER TABLE `prodotti`
+  ADD CONSTRAINT `prodotti_ibfk_1` FOREIGN KEY (`nome_categoria`) REFERENCES `categorie` (`nome`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `utenti_registrati`
+--
+ALTER TABLE `utenti_registrati`
+  ADD CONSTRAINT `utenti_registrati_ibfk_1` FOREIGN KEY (`id_datianagrafici`) REFERENCES `dati_anagrafici` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `valori`
+--
+ALTER TABLE `valori`
+  ADD CONSTRAINT `valori_ibfk_1` FOREIGN KEY (`id_attributo`) REFERENCES `attributi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `valori_ibfk_2` FOREIGN KEY (`id_opzione`) REFERENCES `opzioni` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `valori_ibfk_3` FOREIGN KEY (`id_prodotto`) REFERENCES `prodotti` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
