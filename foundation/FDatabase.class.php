1b3b73dc87fc2e42f2a028a14c64cf4e06b7e52d
Daniele2712 <daniele27_12@hotmail.it>
2018-05-21 17:51:04 +0200

    fix

---
+++ foundation/FDatabase.class.php
@@ -0,0 +1,29 @@
+<?php
+class FDatabase{
+	//Attributi
+	 private $_connection;
+    //Costruttori
+    public function __construct() {
+        
+        global $config; 
+        $this->connect( $config['mysql']['host'], 
+                        $config['mysql']['user'], 
+                        $config['mysql']['password'], 
+                        $config['mysql']['database'] );
+    }
+    //Metodi
+    private function connect( $host, $user, $password, $database ) {
+
+        $this->_connection = new mysqli($host,$user,$password,$database);
+        if ($this->_connection->connect_errno) {
+            echo "Failed to connect to MySQL: (" . $this->_connection->connect_errno . ") " . $this->_connection->connect_error;
+        }
+
+        return true;
+
+    }
+    public function close() {
+        $this->_connection->close();
+    }
+}
+?>
