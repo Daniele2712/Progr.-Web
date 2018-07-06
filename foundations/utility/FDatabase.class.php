ebc2d703bce793c8b64029beccff7f6f2446671a
Byte_Predator <mattia_paris@hotmail.it>
2018-07-06 05:34:01 +0200

    aggiunte transazioni mySQL

--- foundations/utility/FDatabase.class.php
+++ foundations/utility/FDatabase.class.php
@@ -24,26 +24,49 @@ class FDatabase{
 
     public function query($query){
         if(!$this->connection)
-            throw new \SQLException("Error Not Connected", $query, "", 1);
+            throw new \SQLException("Error Not Connected", "", "", 1);
         return $this->connection->query($query);
     }
 
     public function lastId(){
         if(!$this->connection)
-            throw new \SQLException("Error Not Connected", $query, "", 1);
+            throw new \SQLException("Error Not Connected", "", "", 1);
         return $this->connection->insert_id;
     }
 
     public function prepare($query){
         if(!$this->connection)
-            throw new \SQLException("Error Not Connected", $query, "", 1);
+            throw new \SQLException("Error Not Connected", "", "", 1);
         $r = $this->connection->prepare($query);
         if(!$r)
             throw new \SQLException("Error Prepared Statement", $query, $this->error(), 2);
         return $r;
     }
 
+    public function begin_transaction(){
+        if(!$this->connection)
+            throw new \SQLException("Error Not Connected", "", "", 1);
+        if(!$this->connection->begin_transaction())
+            throw new \SQLException("Error Begin Transaction", "", $this->error(), 5);
+    }
+
+    public function commit(){
+        if(!$this->connection)
+            throw new \SQLException("Error Not Connected", "", "", 1);
+        if(!$this->connection->commit())
+            throw new \SQLException("Error Commit", "", $this->error(), 6);
+    }
+
+    public function rollback(){
+        if(!$this->connection)
+            throw new \SQLException("Error Not Connected", "", "", 1);
+        if(!$this->connection->rollback())
+            throw new \SQLException("Error Rollback", "", $this->error(), 7);
+    }
+
     public function error(){
+        if(!$this->connection)
+            throw new \SQLException("Error Not Connected", "", "", 1);
         return $this->connection->error;
     }
 }
