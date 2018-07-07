92c37bd89746bf7645fde6869e9cafb2901f7a28
Daniele2712 <daniele27_12@hotmail.it>
2018-07-07 18:22:59 +0200

    scritto metodo findClosestTo in Magazzino

--- foundations/Magazzino.class.php
+++ foundations/Magazzino.class.php
@@ -24,6 +24,31 @@ class Magazzino extends Foundation{
     }
 
     public static function findClosestTo($addr){
-        
+        $dis="0 km";
+        $arr=array();
+        $mag_arr=array();
+        $result = \Singleton::DB()->query("SELECT id_indirizzo FROM magazzini");
+        if($result){
+            while($result){
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                $id[] = $row["id"];
+            }
+            $mag_arr=Self->findMany($id);
+            while($mag_arr){
+                $ind = $mag_arr[]->getIndirizzo();
+                $url='https://maps.googleapis.com/maps/api/distancematrix/json?origins='
+                    .urlencode($addr->getVia()).','.urlencode($addr->getComune()->getNome()).
+                    ',Italia&destinations='
+                    .urlencode($ind->getVia()).','.urlencode($ind->getComune()->getNome()).
+                    ',Italia&mode=driving&language=it';
+                $data = file_get_contents($url);
+                $arr = json_decode($data, true);
+                $ret = $arr["rows"][0]["elements"][0]["distance"]["text"];
+                if($dis="0 km" || $ret<$dis)
+                   $dis=$ret;
+                   $mag_fin=$mag_arr[];
+            }
+        }
+        return $mag_fin;
     }
 }
