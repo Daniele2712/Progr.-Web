7055dab61cc16bc6df8ea02b0fde18f32309e88d
byte_predator <mattia_paris@hotmail.it>
2019-06-10 19:26:01 +0200

    check automatici per l'install

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 if(file_exists('install/install.php')){
+    $index = is_writable(__FILE__);
+    $install = removableDirectory("install");
+    $includes = is_writable("includes/config.inc.php");
+    $smarty = is_writable("libs/smarty/templates_c");
+    $ok = $index && $install && $includes && $smarty;
 ?>
 <head>
 <title>Installation</title>
@@ -15,6 +20,12 @@ justify:center;
   width:500px;
   max-width:100%;
   text-align: center;
+}
+.error{
+    color: red;
+}
+.ok{
+    color: green;
 }
 form{
   border:2px solid grey;
@@ -54,15 +65,47 @@ h3{
       <input type="text" name="DBusername"><br>
       DB Password:<br>
       <input type="text" name="DBpassword"><br><br>
-      <input type="submit" value="SETUP">
+      <input type="submit" value="SETUP"
+      <?php if(!$ok) echo " disabled";?>
+      >
     </form>
     <h4>The setup might take several minutes, just be patient</h4>
     <p>Before starting remember the following things:</p>
     <ul>
-     <li>index.php must be writable and executable</li>
-     <li>install folder and all its content must be writable and executable</li>
-     <li>includes/config.inc.php must be writable</li>
-     <li>libs/smarty/templates_c must be writable</li>
+     <li>
+     <?php
+        if(!$index){
+            echo "<span class='error'>index.php is not writable</span>";
+        }else{
+            echo "<span class='ok'>index.php is writable</span>";
+        }
+     ?>
+     </li>
+     <li>
+     <?php
+        if(!$install){
+            echo "<span class='error'>install folder and all its content are not writable</span>";
+        }else{
+            echo "<span class='ok'>install folder and all its content are writable</span>";
+        }
+     ?>
+     </li>
+     <li>
+     <?php
+        if(!$includes){
+            echo "<span class='error'>includes/config.inc.php is not writable</span>";
+        }else{
+            echo "<span class='ok'>includes/config.inc.php is writable</span>";
+        }
+     ?></li>
+     <li>
+     <?php
+        if(!$smarty){
+            echo "<span class='error'>libs/smarty/templates_c is not writable</span>";
+        }else{
+            echo "<span class='ok'>libs/smarty/templates_c is writable</span>";
+        }
+     ?></li>
     </ul>
   </div>
 </body>
@@ -70,5 +113,20 @@ h3{
 }
 else{
   echo "Il file che stai cercando e' stato gia' cancellato.";
+}
+
+function removableDirectory($dir){
+    if (!file_exists($dir))
+        return is_writable($dir) && is_executable($dir);
+
+    if (!is_dir($dir))
+        return is_writable($dir);
+
+    foreach (scandir($dir) as $item){
+        if ($item == '.' || $item == '..')
+            continue;
+        return removableDirectory($dir . DIRECTORY_SEPARATOR . $item);
+    }
+    return rmdir($dir);
 }
 ?>
